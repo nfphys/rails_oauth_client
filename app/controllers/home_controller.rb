2727c2024fe12915b4a94488fd175ba6d5b6c369
nfphys <nfphys@gmail.com>
2022-10-17 15:47:36 +0900

    Get OAuth Tokenボタンを実装

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,24 @@
 class HomeController < ApplicationController
   def index
+    @authorization_endpoint = authorization_endpoint
   end
+
+  private 
+
+    def authorization_endpoint
+      query = URI.encode_www_form({
+        response_type: 'code',
+        scope: 'foo',
+        client_id: 'oauth-client',
+        redirect_uri: 'http://localhost:3000/callback',
+        state: SecureRandom.urlsafe_base64,
+      })
+
+      URI::HTTP.build({
+        host: 'localhost',
+        port: 3001,
+        path: '/oauth/authorizations/new',
+        query: query,
+      }).to_s
+    end
 end
