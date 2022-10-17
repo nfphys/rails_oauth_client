589ba6ee3692d4e9f59a312f4e57bf65778d393a
nfphys <nfphys@gmail.com>
2022-10-17 16:13:57 +0900

    authorization_endpoint_uriメソッドを追加

--- app/controllers/oauth/authorizations_controller.rb
+++ app/controllers/oauth/authorizations_controller.rb
@@ -1,20 +1,25 @@
 class Oauth::AuthorizationsController < ApplicationController
   def new
-    query = URI.encode_www_form({
-      response_type: 'code',
-      scope: 'foo',
-      client_id: 'oauth-client',
-      redirect_uri: 'http://localhost:3000/callback',
-      state: SecureRandom.urlsafe_base64,
-    })
-
-    uri = URI::HTTP.build({
-      host: 'localhost',
-      port: 3001,
-      path: '/oauth/authorizations/new',
-      query: query,
-    })
-
+    uri = authorization_endpoint_uri
     redirect_to uri.to_s
   end
+
+  private 
+
+    def authorization_endpoint_uri
+      query = URI.encode_www_form({
+        response_type: 'code',
+        scope: 'foo',
+        client_id: 'oauth-client',
+        redirect_uri: 'http://localhost:3000/callback',
+        state: SecureRandom.urlsafe_base64,
+      })
+  
+      uri = URI::HTTP.build({
+        host: 'localhost',
+        port: 3001,
+        path: '/oauth/authorizations/new',
+        query: query,
+      })
+    end    
 end
