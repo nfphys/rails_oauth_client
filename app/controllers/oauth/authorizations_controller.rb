dea4bdc4ec4d5348f83bc7b24ef15511bdb6538f
nfphys <nfphys@gmail.com>
2022-10-17 16:11:35 +0900

    OAuth::Authorizationsコントローラを作成

---
+++ app/controllers/oauth/authorizations_controller.rb
@@ -0,0 +1,20 @@
+class Oauth::AuthorizationsController < ApplicationController
+  def new
+    query = URI.encode_www_form({
+      response_type: 'code',
+      scope: 'foo',
+      client_id: 'oauth-client',
+      redirect_uri: 'http://localhost:3000/callback',
+      state: SecureRandom.urlsafe_base64,
+    })
+
+    uri = URI::HTTP.build({
+      host: 'localhost',
+      port: 3001,
+      path: '/oauth/authorizations/new',
+      query: query,
+    })
+
+    redirect_to uri.to_s
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
+
+  namespace :oauth do
+    resources :authorizations, only: [:new]
+  end
 end
