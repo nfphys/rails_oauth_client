83d10ae6497b7904d95c2897b6a30c1bfe90b6b9
nfphys <nfphys@gmail.com>
2022-10-20 20:03:17 +0900

    fetch_access_tokenメソッドを追加

--- app/controllers/oauth/callbacks_controller.rb
+++ app/controllers/oauth/callbacks_controller.rb
@@ -6,6 +6,30 @@ class Oauth::CallbacksController < ApplicationController
       return
     end
 
+    res = fetch_access_token
+    puts ""
+    puts "response body: #{res.body}"
+    puts ""
+
     redirect_to root_path
   end
+
+  private
+
+    def fetch_access_token
+      uri = URI.parse('http://localhost:3001/oauth/tokens')
+
+      req = Net::HTTP::Post.new(uri.path)
+      req['Content-Type'] = 'application/x-www-form-urlencoded'
+      req['Authorization'] = "Basic oauth-client:secret"
+      req.set_form_data({
+        'grant_type' => 'authorization_code',
+        'code' => params[:code],
+        'redirect_uri' => oauth_callback_url
+      })
+
+      Net::HTTP.new(uri.host, uri.port).start { |http|
+        http.request(req)
+      }
+    end
 end
